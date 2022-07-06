4a612a9904c7e64d452e5763a22eec7c600f5624
krasm <mkrasowski@gmail.com>
2022-07-06 16:32:38 +0100

    Update GoodQueries.java

--- src/main/java/GoodQueries.java
+++ src/main/java/GoodQueries.java
@@ -2,6 +2,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class GoodQueries implements IQuery {
+    private final PreparedStatememtn accounts;
+    
+    public GoodQueries(Usser usser) {
+        accounts = usser.connection.con.prepareStatement("SELECT * FROM BD4_RACHUNKI WHERE nr_klienta = ?");
+    }
+    
+    
     @Override
     public ResultSet selectQ(String query, Usser usser) throws SQLException {
         return usser.connection.con.prepareStatement(query).executeQuery();
@@ -13,5 +20,18 @@ public class GoodQueries implements IQuery {
         usser.connection.con.prepareStatement(query).executeQuery();
     }
 
-
+    public void List<CustomerAccount> getCustomerAccounts(String customerId) {
+        accounts.setString(1, customerId);
+        ResultSet rs = accounts.executeQuery();
+        List<CustoemrAccount> result = new ArrayList<>();
+        while(rs.next()) {
+            Double accountId = result.getDouble("nr_rachunku");
+            ....
+                
+            CustomerAccount ca = new CustomerAccount(accountId, ...);
+            result.add(ca);
+        }
+        
+        return result;
+    }
 }
